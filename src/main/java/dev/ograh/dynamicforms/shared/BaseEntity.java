eeb941c226a03ef1e7947f1b77b6f38410a296da
Bernard Ograh <ograhbernard@gmail.com>
2026-06-10 23:29:23 +0000

    fix: enable JPA auditing to populate created_at and updated_at on persist

--- src/main/java/dev/ograh/dynamicforms/shared/BaseEntity.java
+++ src/main/java/dev/ograh/dynamicforms/shared/BaseEntity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
