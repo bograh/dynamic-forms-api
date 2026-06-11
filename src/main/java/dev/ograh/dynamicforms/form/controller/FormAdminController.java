bb65938c155ec6af74e5da4f7472f33a6250a7ee
Bernard Ograh <ograhbernard@gmail.com>
2026-06-11 09:23:34 +0000

    fix: update @PreAuthorize to use hasAuthority instead of hasRole

--- src/main/java/dev/ograh/dynamicforms/form/controller/FormAdminController.java
+++ src/main/java/dev/ograh/dynamicforms/form/controller/FormAdminController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/forms")
-@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasAuthority('ADMIN')")
 @RequiredArgsConstructor
 public class FormAdminController {
 
